30e8469aebfeaa34c647ded56685da897df82be1
Sukhrat <sukhrat.arziyev@gmail.com>
2015-06-18 14:03:03 +0600

    added detailed view for images

---
+++ DetailViewController.h
@@ -0,0 +1,16 @@
+//
+//  DetailViewController.h
+//  
+//
+//  Created by Sukhrat on 18.06.15.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailViewController : UIViewController
+
+@property (nonatomic) NSDictionary *photo;
+
+
+@end
